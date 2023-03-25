04759cbb6fc6b9c83b2d712bf01539db00b60270
andre <andreyga22@gmail.com>
2023-03-25 15:06:24 -0600

    testito

--- resources/views/actives/index.blade.php
+++ resources/views/actives/index.blade.php
@@ -7,6 +7,7 @@
     <h1>Activos</h1>
     @foreach($actives as $active)
         <h1>Hola esto es una prueba</h1>
+{{--        testito--}}
         <h2>{{ $active->name }}</h2>
     @endforeach
 @endsection
