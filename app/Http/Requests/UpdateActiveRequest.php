c072500a45579b20e36134e8be30b0676e46ca55
andre <andreyga22@gmail.com>
2023-03-28 22:01:34 -0600

    Aprobado request de los objetos

--- app/Http/Requests/UpdateActiveRequest.php
+++ app/Http/Requests/UpdateActiveRequest.php
@@ -11,7 +11,7 @@ class UpdateActiveRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateDetailRequest.php
+++ app/Http/Requests/UpdateDetailRequest.php
@@ -11,7 +11,7 @@ class UpdateDetailRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
