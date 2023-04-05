3678c395faf3af6cdb7498247ab3f43140dfef8a
marco <marcovrivera03@gmail.com>
2023-04-04 19:00:06 -0600

    button quitado(linea 10)

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -3,9 +3,57 @@
 @section('title', 'Empleados')
 
 @section('content')
-<h2>test</h2>
-    <h1>Empleados</h1>
-    @foreach($employees as $employee)
-        <h2>{{ $employee->firstName }}</h2>
-    @endforeach
+    <h1 class="tituloPrincipal">Empleados</h1>
+
+    <div class="row">
+        <div class="col-sm-auto">
+            <a type="button" class="btn btn-primary btn-create" href="{{route('employee.create')}}">Agregar</a>
+        </div>
+    </div>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">FirtName</th>
+            <th scope="col">SecondName</th>
+            <th scope="col">FirstLastName</th>
+            <th scope="col">SecondLastName</th>
+            <th scope="col">Age</th>
+            <th scope="col">NationalId</th>
+            <th scope="col">Status</th>
+            <th scope="col">Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($employees as $employee)
+            <tr>
+                <td>{{$employee->firsName}}</td>
+                <td>{{$employee->secondName}}</td>
+                <td>{{$employee->firstLastName}}</td>
+                <td>{{$employee->secondLastName}}</td>
+                <td>{{$employee->age}}</td>
+                <td>{{$employee->nationalId}}</td>
+                <td>{{$employee->status}}</td>
+
+                <td>
+                    <div class="row">
+                        <div class="col-sm-auto">
+                            <a type="button" class="btn btn-primary btn-accion" href="{{route('employee.show', $employee)}}">Mostrar</a>
+                        </div>
+                        <div class="col-sm-auto">
+                            <a type="button" class="btn btn-primary btn-accion" href="{{ route('employee.edit', $employee) }}">Editar</a>
+                        </div>
+                        <div class="col-sm-auto">
+                            <form action=" {{ route("employees.destroy", $employee) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" class="btn btn-danger btn-accion" value="Eliminar"/>
+                            </form>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
